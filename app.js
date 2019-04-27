7a9312a362e60c27fd6d4ffcc6a133a486936468
Mark de Guzman <mark.dgzman@gmail.com>
2019-04-26 23:26:01 -0700

    First Push
    Includes Node.js configuration and bootstrap template and library.

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require('express')
+var app = express()
+var path = require('path')
+
+app.set('port', (process.env.PORT || 3000))
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', function(request, response) {
+  response.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
